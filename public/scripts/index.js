241f5cc501a07067468822ff62a4073942fe4845
Thaewyn <thaewyn@gmail.com>
2022-01-11 20:54:45 -0500

    added PIXIjs and went through the super basic tutorial, got some ideas for stuff to try. Also fixed some silly mistakes with the login route

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,16 @@
-console.log("loaded the index.js script")
+//console.log("loaded the index.js script");
+
+let app = new PIXI.Application({width:640, height:360});
+
+document.body.appendChild(app.view);
+
+let sprite = PIXI.Sprite.from('images/sample.png');
+
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+
+app.ticker.add((delta) => {
+  elapsed += delta;
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+})
